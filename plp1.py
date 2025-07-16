6023b7794744c9748a0218a55df634fe3a6ecf2f
She-KH <shekinahhannah4@gmail.com>
2025-07-16 20:37:08 +0300

    python calculator

---
+++ plp1.py
@@ -0,0 +1,18 @@
+#am excise from plp
+#making a mini calculator
+# Ask the user to input the numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# math opperations on the numbers
+sum_results = num1 + num2
+difference_results = num1 - num2
+product_results = num1 * num2
+quotient_results = num1 / num2
+
+# show results
+print (f"Results of your two numbers:")
+print (f"Sum: {sum_results}")
+print (f"Difference: {difference_results}")
+print (f"Product: {product_results}")
+print (f"Quotient: {quotient_results}")
